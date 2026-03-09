4d61989f809cc0c347f208bea4a1707c4e99ca0d
binny <binny0x00@gmail.com>
2026-03-09 22:14:22 +0900

    [코어타임 코딩테스트]day1 수정사항 todo

--- week2/core-time-coding-test/list_remove_duplicates_from_sorted_array.py
+++ week2/core-time-coding-test/list_remove_duplicates_from_sorted_array.py
@@ -5,7 +5,7 @@ class Solution:
         current = 1
         input_index = 1
 
-        for current in range(1, len(nums)): # 빈 배열, 길이 1 배열도 안전하게 처리 됨
+        for current in range(1, len(nums)): # 빈 리스트, 길이 1 배열도 안전하게 처리 됨
             if nums[current] != nums[input_index-1]:
                 nums[input_index] = nums[current]
                 input_index +=1
@@ -14,6 +14,10 @@ class Solution:
 
         return k
 
+"""
+리스트와 배열 차이
+
+"""
 """
 nums: 오름차순 정렬된 정수 배열
 원본 배열 내부에서 중복 원소를 제거해야함
