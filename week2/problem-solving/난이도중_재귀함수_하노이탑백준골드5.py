41c94ef0345f1be8095b68fb39cb1725b51f2cb4
binny <binny0x00@gmail.com>
2026-03-11 00:31:51 +0900

    [문제 풀이 과제-난이도 중]하노이 탑 문제풀이 closes #25

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,20 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+n = int(input())
+count = 0
+
+print(2**n-1)
+
+def hanoi(n, start, end, aux):
+  if n<= 20:
+    if n == 1:
+      print(f"{start} {end}")
+      return
+  
+    hanoi(n-1, start, aux, end)
+    print(f"{start} {end}")
+    hanoi(n-1, aux, end, start)
+  
+
+hanoi(n,1,3,2)
