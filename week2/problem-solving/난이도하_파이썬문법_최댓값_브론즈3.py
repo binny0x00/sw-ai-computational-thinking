a10a9cd144e5c622a61e83362c9990d6054c1f5e
binny <binny0x00@gmail.com>
2026-03-11 01:33:06 +0900

    [문제 풀이 과제-난이도 하]최댓값 풀이 closes #17

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,22 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+# nums = []
+# for i in range(9):
+#   nums.append(int(input()))
+
+# temp = nums.copy()
+# temp.sort()
+# print(temp[-1])
+
+# for j in range(9):
+#   if nums[j] == temp[-1]:
+#     print(j+1)
+
+nums = []
+for i in range(9):
+  nums.append(int(input()))
+
+max_num = max(nums)
+
+print(nums.index(max_num)+1)
