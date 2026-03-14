3edc9f3c017bf3008c9ef84659edd752b87b65e9
binny <binny0x00@gmail.com>
2026-03-15 01:07:00 +0900

    [문제 풀이 과제-난이도 중]링크드리스트 문제 풀이-링크드리스트로 구현 #65

--- week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -11,9 +11,10 @@ class Node:
 
 class LinkedList:
     """단순 연결 리스트"""
-    def __init__(self, cursor):
+    def __init__(self, cursor, length):
         self.cursor = cursor
         self.head = None
+        self.length = length
 
     def append(self, data):
         """리스트 끝에 노드 추가"""
@@ -39,35 +40,49 @@ class LinkedList:
         # TODO: 리스트가 비어있으면 head를 new_node로 설정
         if not self.head:
             self.head = new_node
+
+        elif self.cursor == 0:
+            new_node.next = self.head
+            self.head = new_node
+
         # TODO: 커서 전 노드 찾기
         # TODO: 커서에 해당하는 값 찾기
-        else:
+        else :
             current = self.head
-            while current:
+
+            for i in range(self.cursor - 1):
                 current = current.next
-            # 커서 왼쪽 값 구함
-            # 새로운 노드의 next를 커서 왼쪽 값의 next로, 커서 왼쪽 값의 next를 새로운 노드로 변경해야함
+            
+            new_node.next = current.next
+            current.next = new_node
+        
         self.cursor += 1
+        self.length += 1
     
-    def move_left(self):
-        if not self:
-            cursor -= 1
+    def move_left(self):    # self는 LinkedList 객체 자체
+        if self.cursor > 0:
+            self.cursor -= 1
 
     def move_right(self):
-        if not cursor == len(self):
-            cursor += 1
+        if self.cursor < self.length:
+            self.cursor += 1
 
     def delete_left(self):
-        current = self.head
-        if self:
-            if len(self) == 1:
-                self.head = None
-            else :
-                for i in range(self.cursor):
-                    current = current.next
-                # 커서 왼쪽 값을 구한 상태
-                # 커서 왼쪽의 왼쪽 값의 next를 커서가 가리키는 값으로 변경해야 함
-                cursor -= 1
+        if self.cursor == 0:
+            return
+        
+        if self.cursor == 1:
+            self.head = self.head.next
+        else :
+            current = self.head
+
+            for i in range(self.cursor - 2):
+                current = current.next
+            
+            current.next = current.next.next
+        
+        self.cursor -= 1
+        self.length -= 1
 
     def print_list(self):
         """리스트의 모든 값 출력"""
@@ -83,13 +98,13 @@ class LinkedList:
         return values
     
 if __name__ == "__main__":
-    str = sys.stdin.readline().strip()
+    input = sys.stdin.readline().strip()
     m = int(sys.stdin.readline().strip())
 
-    linked_list = LinkedList(len(str))    # cursor 문장의 맨 끝에 위치
+    linked_list = LinkedList(len(input), len(input))    # cursor 문장의 맨 끝에 위치
 
-    for i in range(len(str)):
-        linked_list.append(str[i])
+    for i in range(len(input)):
+        linked_list.append(input[i])
 
     # print("입력 직후 linked_list:")
     # result = linked_list.print_list()
@@ -108,4 +123,4 @@ if __name__ == "__main__":
             linked_list.append_left(item.split(" ")[1])
 
     result = linked_list.print_list()
-    print(f"{result}")
+    print("".join(result))
