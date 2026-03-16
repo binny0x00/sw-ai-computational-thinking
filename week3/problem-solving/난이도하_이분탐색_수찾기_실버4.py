6729ecd66215d6380ce6acc453acc1b82cc57cc5
binny <binny0x00@gmail.com>
2026-03-16 10:40:14 +0900

    [문제 풀이 과제 - 난이도 하]이분탐색_수찾기 문제 풀이 중

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,22 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+
+# 시간초과 -> 이분탐색으로 수정해야 함
+
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+A = sys.stdin.readline().strip().split(" ")
+
+# print(A)
+
+m = int(sys.stdin.readline().strip())
+
+M = sys.stdin.readline().strip().split(" ")
+
+for i in range(len(M)):
+    if M[i] in A:
+        print("1")
+    else:
+        print("0")
