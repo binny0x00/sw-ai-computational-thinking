0f86d5c3952a1c7b5fcf73bf00973322b92290f2
binny <binny0x00@gmail.com>
2026-03-16 15:56:34 +0900

    [문제 풀이 과제 - 난이도 하]큐카드 문제풀이 closes #64

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,23 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from collections import deque
+import sys
+
+def function(queue):
+    while queue:
+        if len(queue) == 1:
+            print(queue.pop())
+            return
+        queue.popleft()
+        queue.append(queue.popleft())
+
+
+n = int(sys.stdin.readline().strip())
+
+queue = deque()
+
+for i in range(1,n+1):
+    queue.append(i)
+
+function(queue)
