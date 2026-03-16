9eaed11179f68739b22c7c53b3e1a97553c52255
binny <binny0x00@gmail.com>
2026-03-16 13:14:44 +0900

    [문제 풀이 과제 - 난이도 중]분할정복_곱셈 문제풀이 closes #68

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,21 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+import sys
+
+def calculate_squre(num, num2, num3):
+    if num2 == 1:
+        return num % num3
+    
+    if num2 == 2:
+        return (num ** 2) % num3
+    
+    if num2 % 2 == 0:
+        return ((calculate_squre(num, num2//2, num3)) ** 2) % num3
+    else:
+        return (num * ((calculate_squre(num, (num2-1)//2, num3)) ** 2)) % num3
+    
+input_list = sys.stdin.readline().strip().split(" ")
+
+squred_num = calculate_squre(int(input_list[0]), int(input_list[1]), int(input_list[2]))
+print(squred_num)
