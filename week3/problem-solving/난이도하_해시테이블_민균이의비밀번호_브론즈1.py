5678d60b1c885093b97d198b7aa1fc6d12c6c717
binny <binny0x00@gmail.com>
2026-03-17 02:32:20 +0900

    [문제 풀이 과제 - 난이도 하]해시테이블 문제 풀이 중

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,19 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+input_list = []
+
+for i in range(n):
+    input_list.append(sys.stdin.readline().strip())
+
+for j in range(n):
+    target = input_list[j]
+
+    # print(target[::-1])
+
+    if target[::-1] in input_list:
+        print(f"{len(target)} {target[len(target)//2]}")
+        break
