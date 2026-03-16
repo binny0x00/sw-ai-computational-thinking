cde14430990aee07e6ad113f1ffbbf2a48a17cab
binny0x00 <binny0x00@gmail.com>
2026-03-17 03:13:35 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+input_list = set([])
+
+for i in range(n):
+    input_list.add(sys.stdin.readline().strip())
+
+for target in input_list:
+    # print(target[::-1])
+
+    if target[::-1] in input_list:
+        print(f"{len(target)} {target[len(target)//2]}")
+        break
