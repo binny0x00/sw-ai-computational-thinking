746532b0e66e89bb4fbd956370aff5f83d31c412
binny <binny0x00@gmail.com>
2026-03-17 19:38:21 +0900

    [문제 풀이 과제 - 난이도 중] 해시테이블 문제 풀이 closes #72

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,35 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+input_list = []
+input_set = set()
+
+# a+b
+added_num = set()
+
+# list와 set에 입력값 할당하기
+for i in range(n):
+    item = int(sys.stdin.readline().strip())
+    input_set.add(item)
+    input_list.append(item)
+
+for x in range(n):
+    for y in range(n):
+        added_num.add(input_list[x]+input_list[y])
+        continue
+
+# print(added_num)
+
+result = 0
+
+for k in input_list:
+    for c in input_list:
+        if k - c in added_num:
+            if result < k:
+                result = k
+
+if result > 0:
+    print(result)
