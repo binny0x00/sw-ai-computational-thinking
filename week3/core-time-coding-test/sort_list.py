a7500d9ad483424802b967b15a1e133256a789e0
binny <binny0x00@gmail.com>
2026-03-20 10:19:36 +0900

    [코어타임 코딩테스트-week4]sort_list 문제풀이(2차)

--- week3/core-time-coding-test/sort_list.py
+++ week3/core-time-coding-test/sort_list.py
@@ -7,14 +7,46 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
 
+        # 1) base case
         if not head:
             return None
+        
+        if not head.next:
+            return head
 
-        while head.next:
-            # 나누고, 계산하고, 합치고
-            if current.val > current.next.val:
-                temp = current
-                current = current.next
-                current.next = temp
+        # 2) slow/fast로 중간 찾기
+        slow = head
+        fast = head.next
 
-            else: current = current.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # 3) 반으로 끊기
+        mid = slow.next
+        slow.next = None
+
+        # 4) 왼쪽 정렬
+        left = self.sortList(head)
+        # 5) 오른쪽 정렬
+        right = self.sortList(mid)
+
+        dummy = ListNode(0)
+        cur = dummy
+
+        while left and right:
+            if left.val <= right.val:
+                cur.next = left
+                left = left.next
+            else:
+                cur.next = right
+                right = right.next
+
+            cur = cur.next
+
+        if left:
+            cur.next = left
+        else:
+            cur.next = right
+
+        return dummy.next
