02211943b678c284bac2cbb2605524ae95cd2ac6
binny <binny0x00@gmail.com>
2026-03-20 20:24:21 +0900

    [week4-basic] 위상 정렬 closes #92

--- week4/basic/06_topological_sort.py
+++ week4/basic/06_topological_sort.py
@@ -40,21 +40,34 @@ def topological_sort(vertices, edges):
         위상 정렬 순서
     """
     # TODO: 그래프와 진입 차수 초기화
-    pass
+    graph = {x: [] for x in range(vertices)}
+    for item in edges:
+        graph[item[0]].append(item[1])
+
+    degree = [0 for x in range(vertices)]
     
     # TODO: 그래프 구성 및 진입 차수 계산
-    pass
-    
-    # TODO: 진입 차수가 0인 정점들을 큐에 추가
-    pass
-    
+    for item in edges:
+        degree[item[1]] += 1
+
+    q = deque()
+    for i in range(vertices):
+        if degree[i] == 0:
+            q.append(i)
+
     result = []
     
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들의 진입 차수 감소
-    pass
-    
+    while q:
+        target = q.popleft()
+        result.append(target)
+        for i in graph[target]:
+            degree[i] -= 1
+            if degree[i] == 0:
+                q.append(i)
+        
     return result
 
 # 테스트 케이스
