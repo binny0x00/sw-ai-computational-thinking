8037c07fdbfae216b96e9cd53ee11949d98667a4
binny <binny0x00@gmail.com>
2026-03-20 21:01:15 +0900

    [week4-problem] 규칙찾기 closes #93

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,11 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+import sys
+
+t = int(sys.stdin.readline().strip())
+for i in range(t):
+    n,m = sys.stdin.readline().strip().split(" ")
+    for j in range(int(m)):
+        input = sys.stdin.readline().strip()
+    print(int(n)-1)
