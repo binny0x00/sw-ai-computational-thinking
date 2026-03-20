293fd26ff8b08c0361af0a55a9252945c8b771b5
binny0x00 <binny0x00@gmail.com>
2026-03-20 20:52:36 +0900

    [Silver IV] Title: 상근이의 여행, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,8 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+for i in range(t):
+    n,m = sys.stdin.readline().strip().split(" ")
+    for j in range(int(m)):
+        input = sys.stdin.readline().strip()
+    print(int(n)-1)
