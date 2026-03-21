606c0344af2f559302c6151c3812d0bf88352e86
binny0x00 <binny0x00@gmail.com>
2026-03-21 11:03:04 +0900

    [Silver IV] Title: 트리 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14244. 트리 만들기/트리 만들기.py
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.py
@@ -1,21 +1,17 @@
 import sys
 
 # n개의 노드, m개의 리프트로 이루어져 있는 트리
-n, m = map(int, sys.stdin.readline().strip().split(" "))
+n, m = map(int, sys.stdin.readline().strip().split())
 
 graph = []
 
-for i in range(1,m+1):
-    graph.append((0,i))
+for i in range(n-m):
+    graph.append((0+i,i+1))
 
-for j in range(m+1, n):
-    graph.append((j-1, j))
+last_parent_node = graph[-1][1]
 
-# 남은 정점들은 마지막 리프 뒤에 차례대로 연결
-last = m
-for j in range(m + 1, n):
-    graph.append((last, j))
-    last = j
+for j in range(1,m):
+    graph.append((last_parent_node, last_parent_node+j))
 
 for a, b in graph:
     print(a, b)
