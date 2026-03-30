c08a6f6ba9086e85549be3284f250aabc973f7bc
binny0x00 <binny0x00@gmail.com>
2026-03-30 16:51:36 +0900

    [Silver III] Title: 01타일, Time: 136 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,17 @@
+import sys
+
+def dfs(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    
+    a,b = 1,2
+
+    for _ in range(3, n+1):
+        a,b = b, (a+b) % 15746
+    
+    return b
+
+n = int(sys.stdin.readline())
+print(dfs(n))
