31a6287bd74311ee541c64aad44a4d1aa332dbcd
binny <binny0x00@gmail.com>
2026-03-30 11:31:09 +0900

    [week5-basic] greedy 알고리즘 사용해 거스름돈 최소 동전 개수 반환

--- week5/basic/03_greedy_coin.py
+++ week5/basic/03_greedy_coin.py
@@ -46,7 +46,22 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
+    # for i in range(len(coins)):
+    #     count = 0
+    #     coin = coins[i]
+    #     while change >= coin:
+    #         change -= coin
+    #         count += 1
+    #     if count > 0:
+    #         result[coin] = count
+    #         total_coins += count
+
+    for coin in coins:
+        count = change // coin
+        if count >0:
+            result[coin] = count
+            total_coins += count
+            change %= coin
     
     return total_coins, result
 
