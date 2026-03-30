8e6dd0c8f761cdf7f53590f6f6bd17161b464534
binny0x00 <binny0x00@gmail.com>
2026-03-30 20:07:03 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,19 +1,14 @@
 import sys
 
 # [55, 50+40]
-n = sys.stdin.readline().strip().split('-')
+expression = sys.stdin.readline().strip().split('-')
 
 numbers = []
 
-for str in n:
-    if "+" in str:
-        temp = map(int,str.split('+'))
-        sum = 0
-        for num in temp:
-            sum += num
-        numbers.append(sum)
-    else:
-        numbers.append(int(str))
+for group in expression:
+    total = sum(map(int,group.split('+')))
+    numbers.append(total)
+    
 res = numbers[0]
 for i in range(1,len(numbers)):
     res -= numbers[i]
