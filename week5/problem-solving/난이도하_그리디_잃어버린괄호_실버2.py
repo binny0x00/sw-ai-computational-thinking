4bf2e4c1e4641aa0bb0733e8bf81fbeadf917355
binny <binny0x00@gmail.com>
2026-03-30 20:08:46 +0900

    [week5-problem]그리디_괄호 넣어서 최소값 구하기 closes #125

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,19 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+import sys
+
+# [55, 50+40]
+expression = sys.stdin.readline().strip().split('-')
+
+numbers = []
+
+for group in expression:
+    total = sum(map(int,group.split('+')))
+    numbers.append(total)
+    
+res = numbers[0]
+for i in range(1,len(numbers)):
+    res -= numbers[i]
+
+print(res)
