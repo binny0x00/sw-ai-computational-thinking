e9f0e8e9834a5dca2749bcf956067a9d9dd55b2e
binny <binny0x00@gmail.com>
2026-03-30 16:55:37 +0900

    [week5-problem]dp_타일 closes #123

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,20 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+import sys
+
+def dfs(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    
+    a,b = 1,2
+
+    for _ in range(3, n+1):
+        a,b = b, (a+b) % 15746
+    
+    return b
+
+n = int(sys.stdin.readline())
+print(dfs(n))
