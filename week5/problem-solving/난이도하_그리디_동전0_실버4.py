b572ef71fda26906dfe8398006ade5c2dc8cd4b1
binny <binny0x00@gmail.com>
2026-03-30 20:08:16 +0900

    [week5-problem]그리디_동전 closes #124

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,19 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+import sys
+
+n, k = map(int, sys.stdin.readline().strip().split())
+coins = []
+for _ in range(n):
+    coins.append(int(sys.stdin.readline()))
+
+# 큰 순서대로 정렬
+total_count = 0
+
+for coin in reversed(coins):
+    count = k // coin
+    total_count += count
+    k %= coin
+
+print(total_count)
