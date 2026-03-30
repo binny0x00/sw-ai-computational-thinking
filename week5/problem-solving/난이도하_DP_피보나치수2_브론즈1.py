e493bb15d641d26af540aff173bcffe5139fa164
binny <binny0x00@gmail.com>
2026-03-30 13:13:54 +0900

    [week5-problem] dp_피보나치 closes #122

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,17 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+import sys
+
+def fibo(n, memo=None):
+    if memo is None:
+        memo = {0:0, 1:1}
+
+    if n in memo:
+        return memo[n]
+    
+    memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
+    return memo[n]
+
+n = int(sys.stdin.readline())
+print(fibo(n))
